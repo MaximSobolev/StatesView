55add9aa6f0dce34af31642470a250957e479523
MaximSobolev <maximsobolev98@yandex.ru>
2023-01-10 21:03:12 +0500

    Create animation

--- app/src/main/java/ru/netology/statesview/MainActivity.kt
+++ app/src/main/java/ru/netology/statesview/MainActivity.kt
@@ -9,17 +9,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<StatesView>(R.id.statesView).data =
-            mapOf(
-                Pair(StatesViewKey.FULL_AMOUNT, listOf(4000F)),
-                Pair(
-                    StatesViewKey.DATA,
-                    listOf(
-                        1000F,
-                        1000F,
+        val view = findViewById<StatesView>(R.id.statesView)
+        view.postDelayed( {
+            view.data =
+                mapOf(
+                    Pair(StatesViewKey.FULL_AMOUNT, listOf(4000F)),
+                    Pair(
+                        StatesViewKey.DATA,
+                        listOf(
+                            1000F,
+                            1000F,
+                            1000F,
+                            1000F
+                        )
                     )
                 )
-            )
 
+        }, 300)
     }
 }
